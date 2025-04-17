fa83a7d75bb744ce607d60ee2155597612b65f13
Joven Tan <jovenntann@gmail.com>
2025-04-17 11:02:48 +0800

    Update body element class in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>
+      <html lang="en" suppressHydrationWarning>
+        <body className="h-full">
           <Provider>
             {children}
           </Provider>
